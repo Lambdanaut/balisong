49ce681bef9356d8bba402ed2605af61dfa6bbe1
Josh Thomas <personettelabs@gmail.com>
2015-02-25 12:43:11 -0500

    Adds websocket client

---
+++ client/src/resources/js/main.js
@@ -0,0 +1,31 @@
+function WebSocketTest()
+{
+    if ("WebSocket" in window)
+    {
+        console.log("WebSocket is supported by your Browser!");
+        // Let us open a web socket
+        var ws = new WebSocket("ws://localhost:8080");
+        ws.onopen = function()
+        {
+                // Web Socket is connected, send data using send()
+                ws.send("Message to send");
+                console.log("Message is sent...");
+        };
+        ws.onmessage = function (evt) 
+        { 
+                var received_msg = evt.data;
+                console.log("Message is received...");
+        };
+        ws.onclose = function()
+        { 
+                // websocket is closed.
+                console.log("Connection is closed..."); 
+        };
+    }
+    else
+    {
+        // The browser doesn't support WebSocket
+        console.log("WebSocket NOT supported by your Browser!");
+    }
+}
+WebSocketTest();
